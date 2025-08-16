2e0773153ea035017987505b449ccde15113653f
sobz2019 <sobz87@gmail.com>
2025-08-16 10:31:40 +0100

    cleaning the code

--- models/staging/stg_aisles.sql
+++
@@ -1,2 +0,0 @@
-SELECT aisle_id, aisle AS aisle_name
-FROM {{ source('instacart_raw','aisles') }}
--- models/staging/stg_departments.sql
+++
@@ -1,2 +0,0 @@
-SELECT department_id, department AS department_name
-FROM {{ source('instacart_raw','departments') }}
